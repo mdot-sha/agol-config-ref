05b616608392c0f7a96db4661e954388b494ed23
Elliott Plack <talllguy@users.noreply.github.com>
2023-05-19 09:25:17 -0400

    Create groupBy_Outfall.js

---
+++ dashboard/groupBy_Outfall.js
@@ -0,0 +1,20 @@
+// Groups outfall assessments by unique structure number
+
+var portal = Portal('https://www.arcgis.com/');
+var fs = FeatureSetByPortalItem(
+    portal,
+    '0f6acdd5b0f24f239dcddef43635c8b9',
+    0,
+    [
+        'REGIME',
+        'REL_SHA_STR_NO',
+        'REL_STRUCTURE_ID',
+        'REL_GLOBALID'
+    ],
+    false
+);
+
+return GroupBy(fs, ['REGIME','REL_SHA_STR_NO'], 
+[{name: 'total_str_no', expression: 'REL_SHA_STR_NO', statistic: 'COUNT' }, 
+ {name: 'total_str_id', expression: 'REL_STRUCTURE_ID', statistic: 'COUNT' },
+ {name: 'total_guid', expression: 'REL_GLOBALID', statistic: 'COUNT' }]); 
